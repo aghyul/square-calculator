790041d00ec755b647c87cfda32ec301c2dc807e
Nick's Pizza <73615588+azzipskcin@users.noreply.github.com>
2021-11-14 08:55:33 +0100

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+using namespace std;
+
+int main(int argc, char** argv) {
+	int x,carre;
+
+	using namespace std;
+	
+	cout<<"Veulliez Saisir un Entier"<<endl;
+	cin>>x;
+	carre=x*x;
+	
+	cout<<"Le carre est de "<< x <<" est de "<<carre << endl; 
+
+    system("PAUSE");
+	//return EXIT_SUCCESS;
+}
